f3b31b8f97a03fed798d1cf1e55b04e642dc532a
Harkaitz482 <harkatrujillo@hotmail.com>
2024-02-23 19:56:41 +0000

    server things

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -67,7 +67,7 @@ export function AuthProvider({ children }) {
   const postApuestas = async (apuestaData) => {
     try {
       await csrf(); // Asegúrate de tener definida la función csrf
-      const response = await postApiData("/api/apuestas", apuestaData);
+      const response = await postApiData("https://harkaitz.informaticamajada.es/api/apuestas", apuestaData);
       return response;
     } catch (error) {
       console.error("Error creating apuesta:", error);
@@ -78,7 +78,7 @@ export function AuthProvider({ children }) {
   const postPartido = async (partidoData) => {
     try {
       await csrf(); // Asegúrate de tener definida la función csrf
-      const response = await postApiData("/api/partidos", partidoData);
+      const response = await postApiData("https://harkaitz.informaticamajada.es/api/partidos", partidoData);
       return response;
     } catch (error) {
       console.error("Error creating apuesta:", error);
@@ -89,7 +89,7 @@ export function AuthProvider({ children }) {
   // Función para obtener el usuario
   const getUser = async () => {
     try {
-      const data = await getApiData("/api/user");
+      const data = await getApiData("https://harkaitz.informaticamajada.es/api/user");
       setUser(data);
       setSessionVerified(true);
       window.localStorage.setItem(SESSION_NAME, "true");
