e1c72fc643b2b18c8e3b7d0323e8c8efbe8e8710
Harkaitz482 <harkatrujillo@hotmail.com>
2024-02-23 18:24:00 +0000

    Register

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -105,7 +105,7 @@ export function AuthProvider({ children }) {
     setLoading(true);
     try {
       await csrf();
-      await axios.post("/login", data);
+      await axios.post("https://harkaitzreact.informaticamajada.es/login", data);
       await getUser();
     } catch (e) {
       if (typeof e === "object" && e !== null && "response" in e) {
@@ -123,7 +123,7 @@ export function AuthProvider({ children }) {
     setLoading(true);
     try {
       await csrf();
-      await axios.post("/register", data);
+      await axios.post("https://harkaitzreact.informaticamajada.es/register", data);
       await getUser();
     } catch (e) {
       if (typeof e === "object" && e !== null && "response" in e) {
